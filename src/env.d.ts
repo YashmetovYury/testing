f6ce987e57f3b5809896f3e676532c4d6ecb2786
Yashmetov Yury <Yashmetov.Yury@gmail.com>
2023-05-05 07:13:30 +0300

    7

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="astro/client" />
-/// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-	readonly GITHUB_TOKEN: string | undefined;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
